6b770ad72acc239feb277cd1c9d8d2aaa55e496e
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-09-29 15:19:31 -0400

    test useOnClickOutside hook

--- part-1/src/__tests__/unit/hooks/case-7-useOutsideClick.test.tsx
+++ part-1/src/__tests__/unit/hooks/case-7-useOutsideClick.test.tsx
@@ -8,7 +8,37 @@ describe('The useOutsideClick hook', () => {
   // Very important to make sure the component is used the right way.
   // This test can be migrated in future, if the useOutsideClick
   // hook is moved to an external library at your company.
-  it('❌ calls the outside click handler when an outside click is initiated', () => {})
+  const Panel = () => {
+    const ref = useRef(null)
+    const [showing, setShowing] = useState(false)
+
+    useOutsideClick(ref, () => {
+      setShowing(!showing)
+    })
+
+    return (
+      <div ref={ref}>
+        <button data-testid="PanelButton">
+          {showing ? 'SHOWING BUTTON' : 'HIDING BUTTON'}
+        </button>
+      </div>
+    )
+  }
+
+  it('calls the outside click handler when an outside click is initiated', () => {
+    const { getByTestId } = render(
+      <div>
+        <button data-testid="ButtonOutsidePanel">
+
+        </button>
+        <Panel />
+      </div>
+    )
+
+    fireEvent.click(getByTestId('ButtonOutsidePanel'))
+
+    expect((getByTestId('PanelButton'))).toHaveTextContent('SHOWING BUTTON')
+  })
 
   it('❌ cleans up the event listeners after component is unmounted', () => {})
 })
